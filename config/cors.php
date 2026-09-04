ff36b70d81403ec0e7c1b0747026883755afc694
the-sann <kimsannmao025@gmail.com>
2026-09-04 18:36:53 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://192.168.80.137:8080'],
+    'allowed_origins' => ['http://192.168.80.137:3000'],
 
     'allowed_origins_patterns' => [],
 
